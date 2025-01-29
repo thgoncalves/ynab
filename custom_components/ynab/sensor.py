7b3de134b39d470e706b89248e323080b2005c48
thiagopinto-42q <thiago@42q.app>
2025-01-28 17:08:52 -0700

    Converting attributes to sensor

--- custom_components/ynab/sensor.py
+++ custom_components/ynab/sensor.py
@@ -1,4 +1,5 @@
 """Sensor platform for ynab."""
+
 import logging
 from homeassistant.helpers.entity import Entity
 
@@ -7,107 +8,137 @@ from .const import ACCOUNT_ERROR, CATEGORY_ERROR, DOMAIN_DATA, ICON
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(
-    hass, config, async_add_entities, discovery_info=None
-):  # pylint: disable=unused-argument
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up sensor platform."""
-    async_add_entities([ynabSensor(hass, discovery_info)], True)
+    sensors = []
+
+    # Fetch YNAB data
+    await hass.data[DOMAIN_DATA]["client"].update_data()
+
+    # Create main budget sensors
+    main_sensors = [
+        "budgeted_this_month",
+        "activity_this_month",
+        "age_of_money",
+        "total_balance",
+        "need_approval",
+        "uncleared_transactions",
+        "overspent_categories",
+    ]
+
+    for sensor_name in main_sensors:
+        sensors.append(YNABSensor(hass, sensor_name, config["currency"]))
+
+    # Create category sensors separately
+    categories = config.get("categories", [])
+    for category in categories:
+        sensors.append(YNABCategorySensor(hass, category, config["currency"]))
+        sensors.append(
+            YNABCategorySensor(hass, f"{category}_budgeted", config["currency"])
+        )
+
+    # Create account sensors
+    accounts = config.get("accounts", [])
+    for account in accounts:
+        sensors.append(YNABAccountSensor(hass, account, config["currency"]))
+
+    async_add_entities(sensors, True)
 
 
-class ynabSensor(Entity):
-    """YNAB Sensor class."""
+class YNABSensor(Entity):
+    """General YNAB Sensor class for main metrics."""
 
-    def __init__(self, hass, config):
-        """Init."""
+    def __init__(self, hass, name, currency):
+        """Initialize the sensor."""
         self.hass = hass
-        self.attr = {}
+        self._name = f"YNAB {name.replace('_', ' ').title()}"
         self._state = None
-        self._name = config["name"]
-        self._measurement = config["currency"]
-        self._categories = config["categories"]
-        self._accounts = config["accounts"]
+        self._measurement = currency
+        self._data_key = name
 
     async def async_update(self):
         """Update the sensor."""
         await self.hass.data[DOMAIN_DATA]["client"].update_data()
-
-        to_be_budgeted = self.hass.data[DOMAIN_DATA].get("to_be_budgeted")
-
-        if to_be_budgeted is not None:
-            self._state = to_be_budgeted
-
-        # set attributes
-        self.attr["budgeted_this_month"] = self.hass.data[DOMAIN_DATA].get(
-            "budgeted_this_month"
-        )
-
-        self.attr["activity_this_month"] = self.hass.data[DOMAIN_DATA].get(
-            "activity_this_month"
-        )
-        self.attr["age_of_money"] = self.hass.data[DOMAIN_DATA].get("age_of_money")
-
-        self.attr["total_balance"] = self.hass.data[DOMAIN_DATA].get("total_balance")
-
-        self.attr["need_approval"] = self.hass.data[DOMAIN_DATA].get("need_approval")
-
-        self.attr["uncleared_transactions"] = self.hass.data[DOMAIN_DATA].get(
-            "uncleared_transactions"
-        )
-
-        self.attr["overspent_categories"] = self.hass.data[DOMAIN_DATA].get(
-            "overspent_categories"
-        )
-
-        # category attributes
-        if self._categories is not None:
-            for category in self._categories:
-                if self.hass.data[DOMAIN_DATA].get(category) is not None:
-                    self.attr[category.replace(" ", "_").lower()] = self.hass.data[
-                        DOMAIN_DATA
-                    ].get(category)
-                    self.attr[
-                        (category + "_budgeted").replace(" ", "_").lower()
-                    ] = self.hass.data[DOMAIN_DATA].get(category + "_budgeted")
-                else:
-                    category_error = CATEGORY_ERROR.format(category=category)
-                    _LOGGER.error(category_error)
-
-        if self._accounts is not None:
-            for account in self._accounts:
-                if self.hass.data[DOMAIN_DATA].get(account) is not None:
-                    self.attr[account.replace(" ", "_").lower()] = self.hass.data[
-                        DOMAIN_DATA
-                    ].get(account)
-                else:
-                    account_error = ACCOUNT_ERROR.format(account=account)
-                    _LOGGER.error(account_error)
-
-    @property
-    def should_poll(self):
-        """Return the name of the sensor."""
-        return True
+        self._state = self.hass.data[DOMAIN_DATA].get(self._data_key)
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
     def unit_of_measurement(self):
-        """Return the unit of measurement of the sensor."""
         return self._measurement
 
     @property
     def icon(self):
-        """Return the icon of the sensor."""
         return ICON
 
+
+class YNABCategorySensor(Entity):
+    """Sensor for individual budget categories."""
+
+    def __init__(self, hass, category, currency):
+        """Initialize the sensor."""
+        self.hass = hass
+        self._name = f"YNAB {category.replace('_', ' ').title()}"
+        self._state = None
+        self._measurement = currency
+        self._category = category
+
+    async def async_update(self):
+        """Update the sensor."""
+        await self.hass.data[DOMAIN_DATA]["client"].update_data()
+        self._state = self.hass.data[DOMAIN_DATA].get(self._category)
+
     @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        return self.attr
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        return self._measurement
+
+    @property
+    def icon(self):
+        return ICON
+
+
+class YNABAccountSensor(Entity):
+    """Sensor for individual accounts in YNAB."""
+
+    def __init__(self, hass, account, currency):
+        """Initialize the sensor."""
+        self.hass = hass
+        self._name = f"YNAB Account {account.replace('_', ' ').title()}"
+        self._state = None
+        self._measurement = currency
+        self._account = account
+
+    async def async_update(self):
+        """Update the sensor."""
+        await self.hass.data[DOMAIN_DATA]["client"].update_data()
+        self._state = self.hass.data[DOMAIN_DATA].get(self._account)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        return self._measurement
+
+    @property
+    def icon(self):
+        return ICON
